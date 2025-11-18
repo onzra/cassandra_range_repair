e416b04fa76efbe21fde24ec99ac838e0589ae8f
bogdan3d <bogdan3d@gmail.com>
2025-11-18 15:11:35 -0500

    84374 Adding critical log of start and end in new try finally block around repair.

--- src/range_repair.py
+++ src/range_repair.py
@@ -961,7 +961,11 @@ def main():
         logging.debug('--resume requires --output-status')
         sys.exit(1)
 
-    repair(options)
+    try:
+        logging.critical('Started ONZRA Cassandra Range Repair')
+        repair(options)
+    finally:
+        logging.critical('Finished ONZRA Cassandra Range Repair ')
     exit(0)
 
 
